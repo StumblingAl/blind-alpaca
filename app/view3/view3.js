b527c19c228a0746081147dfb93c41c3a3cf0c76
StumblingAl <alex@MacBook-Pro.local>
2017-05-15 08:09:24 +0100

    Additon view 3

---
+++ app/view3/view3.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp.view3', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view3', {
+    templateUrl: 'view3/view3.html',
+    controller: 'View3Ctrl'
+  });
+}])
+
+.controller('View3Ctrl', ['$http', function($http) {
+
+    var vc3 = this;
+
+}]);
